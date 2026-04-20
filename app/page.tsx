57e60c94ef41e3f81017fccbde988b7171d8932c
Merlake <matrohin222@gmail.com>
2026-04-20 18:01:16 +0300

    Initial commit Merlake site

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl text-purple-500">
+        Merlake 🚀
+      </h1>
+    </main>
+  );
+}
